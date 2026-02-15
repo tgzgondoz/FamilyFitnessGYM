a0852f1f4350c32e5ed6a691bdcb316effc696c8
tgzgondoz <tgzgondozz@gmail.com>
2026-02-15 08:22:18 +0200

    apk

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,8 @@
 ﻿module.exports = {
   presets: ['babel-preset-expo'],
   plugins: [
-    'react-native-reanimated/plugin' // ← This is where it should be
+    '@babel/plugin-proposal-optional-chaining',
+    '@babel/plugin-transform-optional-chaining',
+    'react-native-reanimated/plugin' // This should be last
   ]
 };
